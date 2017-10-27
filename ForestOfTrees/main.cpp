b3691440a8d5a6fc2007a6c297fa565be626984d
Jshewmaker <jshewmak@gmail.com>
2017-10-27 14:51:40 -0400

    Initial Commit

---
+++ ForestOfTrees/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ForestOfTrees
+//
+//  Created by Joshua Shewmaker on 10/27/17.
+//  Copyright © 2017 Joshua Shewmaker. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
